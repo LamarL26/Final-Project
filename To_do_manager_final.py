7cd1f00554823a98734182f08dd8a8d8db679877
Lamar Logan <llogan12@terpmail.umd.edu>
2025-05-16 19:24:41 -0400

    Finished the code up to line 43 so far, will contine to work on it

--- To_do_manager_final.py
+++ To_do_manager_final.py
@@ -3,3 +3,42 @@
 # INST326
 # Prof. Patrick
 
+import json
+import re
+from datetime import datetime
+
+# Task class defines the structure and behavior of a to-do task
+class Task:
+    """Represents only one to-do task."""
+    def __init__(self, title, description, due_date, priority, completed=False):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.priority = priority
+        self.completed = completed
+
+    def mark_completed(self):
+        # Right here, it will show that the task as completed
+        self.completed = True
+
+    def edit(self, title=None, description=None, due_date=None, priority=None):
+        # This step right here will allow the user to edit the task. 
+        # The user will be able to edit the title, description, due date, and priority of the task.
+        if title:
+            self.title = title
+        if description:
+            self.description = description
+        if due_date:
+            self.due_date = due_date
+        if priority:
+            self.priority = priority
+    
+    def to_dict(self):
+    # Right here, convert the task to dictionary for saving to JSON
+    return self._dict_
+
+    @staticmethod
+    def from_dict(data):
+        # Create a task object from a dictionary here
+        return Task(**data)
+    
